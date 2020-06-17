62486047c7ab3947e5b8091170c58558713d7126
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2020-06-17 10:10:41 +0530

    Update bug.c

--- bug-finding/bug.c
+++ bug-finding/bug.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void print_good()
+/*void print_good()
 {
 	printf("Great!\n");
-}
+}*/
 
 int main(int argc, char const *argv[])
 {
 	char buf[8];
 	printf("Enter password:\n");
-	gets(buf);
+	for(int i=0; i<8;i++)
+		buf[i] = getchar();
 	printf("Try again!\n");
 	return 0;
 }
